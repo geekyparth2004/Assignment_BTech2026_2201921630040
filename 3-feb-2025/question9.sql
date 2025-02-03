f6a9d9ea32a73db7caa3d98d28620e17e58742a0
geekyparth2004 <goelparth20049@gmail.com>
2025-02-03 23:46:15 +0530

    update

---
+++ 3-feb-2025/question9.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Employees (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    dept_id INT
+);
+
+CREATE TABLE Departments (
+    dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
